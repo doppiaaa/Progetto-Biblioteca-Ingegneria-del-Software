fa270f7a6671da5946c8f083629e9cfc04f72142
wakeupb4be <berardino.bruno04@gmail.com>
2025-12-05 20:33:37 +0100

    Update Utente.java

--- src/main/java/softwareeng/biblioteca/model/Utente.java
+++ src/main/java/softwareeng/biblioteca/model/Utente.java
@@ -3,12 +3,17 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package softwareeng.biblioteca.module;
+package softwareeng.biblioteca.model;
 
 /**
  *
  * @author cashrules
  */
 public class Utente {
-    
+    private String nome;
+    private String cognome;
+    private String matricola;
+    private String mail;
+    private ArrayList<Prestito>;
+
 }
