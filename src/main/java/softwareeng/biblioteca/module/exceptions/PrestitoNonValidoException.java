5ca0dbffb09d4d531d1e3e64995e2fdcbdaedcd4
Dino <berardino.bruno04@gmail.com>
2025-12-05 17:20:24 +0100

    creazione scheletro progetto su Netbeans with Maven

---
+++ src/main/java/softwareeng/biblioteca/module/exceptions/PrestitoNonValidoException.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package softwareeng.biblioteca.module.exceptions;
+
+/**
+ *
+ * @author cashrules
+ */
+public class PrestitoNonValidoException extends RuntimeException {
+
+    /**
+     * Creates a new instance of <code>PrestitoNonValidoException</code> without
+     * detail message.
+     */
+    public PrestitoNonValidoException() {
+    }
+
+    /**
+     * Constructs an instance of <code>PrestitoNonValidoException</code> with
+     * the specified detail message.
+     *
+     * @param msg the detail message.
+     */
+    public PrestitoNonValidoException(String msg) {
+        super(msg);
+    }
+}
