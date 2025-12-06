2650c24911dcdc862d3722e004dcd288ae4687a1
Dino <berardino.bruno04@gmail.com>
2025-12-06 17:03:52 +0100

    aggiunto plugin java-fx per Maven nel pom.xml e creata ultima classe MainApp

---
+++ src/main/java/softwareeng/biblioteca/application/MainApp.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package softwareeng.biblioteca.application;
+
+import javafx.application.Application;
+
+/**
+ *
+ * @author cashrules
+ */
+public class MainApp extends Application {
+    
+}
