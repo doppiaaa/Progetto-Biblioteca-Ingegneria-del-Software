689c52a1c09c3e942e3f7e7ab17c9e3c38365cf1
fbacco56tgc-tech <fbacco56.tgc@gmail.com>
2025-12-07 12:32:55 +0100

    Update MainApp.java

--- src/main/java/softwareeng/biblioteca/application/MainApp.java
+++ src/main/java/softwareeng/biblioteca/application/MainApp.java
@@ -12,5 +12,24 @@ import javafx.application.Application;
  * @author cashrules
  */
 public class MainApp extends Application {
+    private Stage primaryStage;
+    private Catalogo catalogo;
+    private ListaUtenti utenti; 
+    private ListaPrestiti prestiti;
+    private GestoreFile gestoreFile;
+
+    @Override
+    public void start(Stage primaryStage);
     
+    public void mostraHome();
+    
+    public void mostraLibri();
+
+    public void mostraUtenti();
+    
+    public void mostraPrestiti();
+    
+    public void caricaDati();
+    
+    public void salvaDati()
 }
