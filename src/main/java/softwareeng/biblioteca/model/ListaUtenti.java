acee980b8fb48691b17e6fc2c24de72ce6f8134e
fbacco56tgc-tech <fbacco56.tgc@gmail.com>
2025-12-07 12:53:12 +0100

    Update ListaUtenti.java

--- src/main/java/softwareeng/biblioteca/model/ListaUtenti.java
+++ src/main/java/softwareeng/biblioteca/model/ListaUtenti.java
@@ -11,28 +11,80 @@ import java.io.Serializable;
  *
  * @author cashrules
  */
+
+/**
+ * @file ListaUtenti.java
+ * @brief File contenente l'implementazione della classe ListaUtenti.
+ * @version 1.0
+ */
+
+/**
+ * @brief Classe concreta per la gestione dell'elenco degli utenti della biblioteca.
+ * * Questa classe implementa l'interfaccia GestioneUtenti e l'interfaccia Serializable
+ * per garantire la persistenza dei dati. Utilizza una ObservableList per permettere
+ * il data-binding automatico con l'interfaccia grafica JavaFX.
+ * * @see GestioneUtenti
+ * @see Utente
+ */
 public class ListaUtenti implements Serializable, GestioneUtenti {
+    /** * @brief Lista osservabile contenente gli utenti registrati.
+     * Viene inizializzata nel costruttore come FXCollections.observableArrayList().
+     */
     private ObservableList<Utente> utenti;
-    
+
+    /**
+     * @brief Costruttore predefinito.
+     * * Inizializza la lista degli utenti come una collezione vuota osservabile.
+     * @post utenti != null && utenti.isEmpty() == true
+     */
     public ListaUtenti(){
     
     }
-    
+
+    /**
+     * @brief Restituisce l'elenco completo degli utenti.
+     * * @return ObservableList<Utente> La lista di tutti gli utenti gestiti.
+     */
     @Override
     public ObservableList<Utente> getElenco(){
         
     }
-    
+
+    /**
+     * @brief Aggiunge un nuovo utente alla lista.
+     * * @param[in] utente L'oggetto Utente da aggiungere.
+     * @pre utente != null
+     * @pre checkID(utente.getMatricola()) == false (L'utente non deve essere già presente)
+     * @post utenti.contains(utente) == true
+     * @post utenti.size() == old(utenti.size()) + 1
+     */
     @Override
     public void aggiungi(Utente utente){
         
     }
-    
+
+    /**
+     * @brief Rimuove un utente dalla lista.
+     * * Verifica che l'utente non abbia prestiti attivi prima di procedere con la rimozione,
+     * per garantire l'integrità referenziale del sistema.
+     * * @param[in] utente L'oggetto Utente da rimuovere.
+     * @pre utente != null
+     * @pre utenti.contains(utente) == true
+     * @pre utente.checkPrestiti() == false (L'utente non deve avere prestiti in corso)
+     * @post utenti.contains(utente) == false
+     * @throws EliminazioneNonValidaException Se l'utente ha prestiti attivi.
+     */
     @Override
     public void rimuovi(Utente utente){
         
     }
-    
+
+    /**
+     * @brief Verifica l'esistenza di un utente tramite il suo identificativo (Matricola).
+     * * Utilizzato per validare l'unicità della matricola durante l'inserimento di nuovi utenti.
+     * * @param[in] id La matricola dell'utente da cercare.
+     * @return true se esiste già un utente con quella matricola, false altrimenti.
+     */
     @Override
     public boolean checkID(String id){
         
