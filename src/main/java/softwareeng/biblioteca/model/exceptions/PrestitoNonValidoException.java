344adefaf3bd67d593db3d98a9ad5552592a81e5
fbacco56tgc-tech <fbacco56.tgc@gmail.com>
2025-12-07 13:47:33 +0100

    Update PrestitoNonValidoException.java

--- src/main/java/softwareeng/biblioteca/model/exceptions/PrestitoNonValidoException.java
+++ src/main/java/softwareeng/biblioteca/model/exceptions/PrestitoNonValidoException.java
@@ -6,23 +6,37 @@
 package softwareeng.biblioteca.model.exceptions;
 
 /**
+ * @file PrestitoNonValidoException.java
+ * @brief File contenente la definizione dell'eccezione PrestitoNonValidoException.
+ * @version 1.0
+ */
+
+/**
+ * @brief Eccezione lanciata quando un tentativo di prestito viola le regole di business.
  *
- * @author cashrules
+ * Questa eccezione viene sollevata dai gestori dei prestiti (vedi GestionePrestiti::addPrestito)
+ * quando non è possibile completare la transazione. Le cause comuni includono:
+ * - Il libro richiesto non ha copie disponibili (Libro::checkDisponibilità restituisce false).
+ * - L'utente ha raggiunto il limite massimo di prestiti attivi o ha sanzioni (Utente::checkDisponibilità restituisce false).
+ *
+ * @see softwareeng.biblioteca.model.GestionePrestiti
  */
 public class PrestitoNonValidoException extends RuntimeException {
 
-    /**
-     * Creates a new instance of <code>PrestitoNonValidoException</code> without
-     * detail message.
+   /**
+     * @brief Costruttore predefinito.
+     *
+     * Crea una nuova istanza di PrestitoNonValidoException senza un messaggio di dettaglio specifico.
      */
     public PrestitoNonValidoException() {
     }
 
-    /**
-     * Constructs an instance of <code>PrestitoNonValidoException</code> with
-     * the specified detail message.
+  /**
+     * @brief Costruttore con messaggio di dettaglio.
      *
-     * @param msg the detail message.
+     * Crea una nuova istanza di PrestitoNonValidoException specificando la causa dell'errore.
+     *
+     * @param[in] msg Il messaggio descrittivo dell'errore (es. "Copie esaurite", "Utente non abilitato").
      */
     public PrestitoNonValidoException(String msg) {
         super(msg);
