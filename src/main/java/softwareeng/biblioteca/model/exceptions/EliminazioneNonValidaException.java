fa02f3b82d08c662be5c7e7bae3e8ce5a67255f7
fbacco56tgc-tech <fbacco56.tgc@gmail.com>
2025-12-07 13:49:10 +0100

    Update EliminazioneNonValidaException.java

--- src/main/java/softwareeng/biblioteca/model/exceptions/EliminazioneNonValidaException.java
+++ src/main/java/softwareeng/biblioteca/model/exceptions/EliminazioneNonValidaException.java
@@ -6,23 +6,43 @@
 package softwareeng.biblioteca.model.exceptions;
 
 /**
+ * @file EliminazioneNonValidaException.java
+ * @brief File contenente la definizione dell'eccezione EliminazioneNonValidaException.
+ * @version 1.0
+ */
+
+/**
+ * @brief Eccezione lanciata quando un tentativo di eliminazione viola i vincoli di integrità.
  *
- * @author cashrules
+ * Questa eccezione (di tipo RuntimeException) viene sollevata dai metodi di rimozione
+ * nelle classi di gestione (es. GestioneLibri::rimuovi, GestioneUtenti::rimuovi) quando
+ * l'entità che si sta cercando di eliminare ha dipendenze attive che non possono essere
+ * ignorate.
+ *
+ * Esempi tipici:
+ * - Tentativo di eliminare un Libro che è attualmente in prestito.
+ * - Tentativo di eliminare un Utente che ha ancora libri in prestito non restituiti.
+ *
+ * @see softwareeng.biblioteca.model.Gestione
  */
 public class EliminazioneNonValidaException extends RuntimeException {
 
     /**
-     * Creates a new instance of <code>EliminazioneNonValidaException</code>
-     * without detail message.
+     * @brief Costruttore predefinito.
+     *
+     * Crea una nuova istanza di EliminazioneNonValidaException senza un messaggio
+     * di dettaglio specifico.
      */
     public EliminazioneNonValidaException() {
     }
 
-    /**
-     * Constructs an instance of <code>EliminazioneNonValidaException</code>
-     * with the specified detail message.
+   /**
+     * @brief Costruttore con messaggio di dettaglio.
      *
-     * @param msg the detail message.
+     * Crea una nuova istanza di EliminazioneNonValidaException specificando la causa
+     * della violazione.
+     *
+     * @param[in] msg Il messaggio descrittivo dell'errore (es. "Prestito Attivo", "Impossibile rimuovere utente con debiti").
      */
     public EliminazioneNonValidaException(String msg) {
         super(msg);
